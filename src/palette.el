c56d089b635c05c4ef1700e6fa5fae6e878723bf
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-20 20:15:05 +0000

    saves palette.el

--- src/palette.el
+++ src/palette.el
@@ -1,4 +1,5 @@
 (defun palette-regexp-to-string ()
+  (save-match-data
   (let* (
          (color-name (match-string 1))
          (color-tone-1 (match-string 5))
@@ -11,8 +12,13 @@
          (red (match-string 15))
          (green (match-string 16))
          (blue (match-string 17))
+         (line-rs (format "(%24s, %S, [%3d, %3d, %3d] ),"
+            (format "%S" color-name)
+            (format "%S" (format "#%s" (string-join (list hex-red hex-green hex-blue) "")))
+            red, green, blue))
          )
     )
+  )
   )
 (defun palette-color-scavenge-buffer (&optional buffer-or-name)
   (interactive)
