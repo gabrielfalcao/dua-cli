4e9a32690b47834c9cfe977d9bad291d59f8512e
Gabriel Falcão <gabrielteratos@gmail.com>
2026-04-12 20:07:54 +0000

    build-armv7.sh

--- build-armv7.sh
+++ build-armv7.sh
@@ -1,2 +1,3 @@
-cargo build --target "armv7-unknown-linux-gnueabihf"
-cargo build --target "armv7-unknown-linux-gnueabi"
+cargo zigbuild --target "armv7-unknown-linux-gnueabihf"
+cargo zigbuild --target "armv7-unknown-linux-gnueabi"
+# npx -y create-rsbuild --dir my-app --template react
