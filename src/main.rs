36f4bd7e1ce4d87e6f8c6c28606636b6e30967de
Gabriel Falcão <gabrielteratos@gmail.com>
2026-06-19 02:14:13 +0000

    starting to figure out how to add the option to make interactive subcommand print size of each marked path

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 #![forbid(rust_2018_idioms, unsafe_code)]
+use crate::interactive::widgets::EntryMark;
 use anyhow::{Context, Result, anyhow, bail};
 use clap::{CommandFactory as _, Parser};
 use dua::{TraversalSorting, canonicalize_ignore_dirs};
 use log::info;
-use std::{
-    fs, io,
-    io::{IsTerminal, Write},
-    path::{Path, PathBuf},
-    process,
-};
+use std::io::{IsTerminal, Write};
+use std::path::{Path, PathBuf};
+use std::{fs, io, process};
 
 #[cfg(feature = "tui-crossplatform")]
 use crate::interactive::input::input_channel;
@@ -93,11 +91,10 @@ fn main() -> Result<()> {
         #[cfg(feature = "tui-crossplatform")]
         Some(Interactive { no_entry_check }) => {
             use anyhow::{Context, anyhow};
-            use crossterm::{
-                execute,
-                terminal::{EnterAlternateScreen, enable_raw_mode},
-            };
-            use tui::{Terminal, backend::CrosstermBackend};
+            use crossterm::execute;
+            use crossterm::terminal::{EnterAlternateScreen, enable_raw_mode};
+            use tui::Terminal;
+            use tui::backend::CrosstermBackend;
 
             let config = dua::Config::load()?;
 
@@ -133,7 +130,22 @@ fn main() -> Result<()> {
                     app.window
                         .mark_pane
                         .take()
-                        .map(|marked| marked.into_paths()),
+                        .map(|marked_widget| {
+                            let marked = marked_widget.marked();
+
+                            for (key, entry) in marked.iter() {
+                                let size = entry.size;
+                                let path = entry.path.canonicalize().unwrap_or_else(|_| entry.path.clone()).display().to_string();
+                                let index = entry.index;
+                                let num_errors_during_deletion = entry.num_errors_during_deletion;
+                                let is_dir = entry.is_dir;
+                                let entry_count = entry.entry_count;
+                                let value = format!("{path} {size}");
+                                eprintln!("\x1b[1;48;2;143;211;255m\x1b[1;38;5;0;0;0m{key:#?}\x1b[0m\x1b[1;48;2;240;79;120m\x1b[1;38;5;0;0;0m{value}\x1b[0m");
+                            }
+                            marked_widget.into_paths()
+
+                        }),
                 )
             });
             // Leak app memory to avoid having to wait for the hashmap to deallocate,
@@ -147,6 +159,7 @@ fn main() -> Result<()> {
             // Exit 'quickly' to avoid having to not have to deal with slightly different types in the other match branches
             std::process::exit(
                 res.map(|(walk_result, paths)| {
+                    // eprintln!("\x1b[1;48;2;240;79;120m\x1b[1;38;5;0;0;0m{walk_result:#?}\x1b[0m");
                     if let Some(paths) = paths {
                         for path in paths {
                             let fallback = path.clone();
@@ -245,12 +258,17 @@ fn cwd_dirlist() -> Result<Vec<PathBuf>, io::Error> {
                 .and_then(|e| e.path().strip_prefix(".").ok().map(ToOwned::to_owned))
         })
         .filter(|p| {
-            if let Ok(meta) = p.symlink_metadata()
-                && meta.file_type().is_symlink()
-            {
-                return false;
-            };
-            true
+            let path = p.clone();
+            match p.symlink_metadata() {
+                Ok(meta) => {
+                    if meta.file_type().is_symlink() {
+                        false
+                    } else {
+                        true
+                    }
+                }
+                Err(_e) => true,
+            }
         })
         .collect();
     v.sort();
